8e59c4ae34a297cb52087639048bdeb1ac48fbe7
Retropaint <darkglasses1122@gmail.com>
2025-03-23 15:15:40 +0700

    added armarture_window and bone_window

--- src/shared.rs
+++ src/shared.rs
@@ -1,12 +1,39 @@
-//! Struct for easily-accessible, frequently-shared data.
+//! Easily-accessible and frequently-shared data
 
 use wgpu::BindGroup;
 
 use crate::Vec2;
+
+#[derive(Clone, Default)]
+pub struct BoneTexture {
+    pub idx: usize, // index relative to skelements texture vector
+}
+
+#[derive(Clone, Default)]
+pub struct Bone {
+    pub name: String,
+    pub parent_id: i32,
+    pub pos: Vec2,
+    pub rot: f32,
+    pub scale: Vec2,
+    pub id: i32,
+    pub tex: BoneTexture,
+
+    // used to properly offset bone's movement to counteract it's parent
+    pub parent_rot: f32,
+}
+
+#[derive(Clone, Default)]
+pub struct Armature {
+    pub bones: Vec<Bone>,
+}
 
 #[derive(Default)]
 pub struct Shared {
     pub mouse: Vec2,
     pub window: Vec2,
     pub bind_groups: Vec<BindGroup>,
+    pub dragging: bool,
+    pub selected_bone: usize,
+    pub armature: Armature,
 }
--- src/ui.rs
+++ src/ui.rs
@@ -2,9 +2,15 @@
 
 use egui::Context;
 
+use crate::{armature_window, bone_window};
+use crate::shared::Shared;
+
 /// The `main` of this module.
-pub fn draw(context: &Context) {
+pub fn draw(context: &Context, shared: &mut Shared) {
     egui::Window::new("lol").show(context, |ui| {
         ui.label("test");
     });
+
+    armature_window::draw(context, shared);
+    bone_window::draw(context, shared);
 }
