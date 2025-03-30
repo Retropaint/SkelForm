54662130ca8d2c6e445b0dccaf4878e53be84164
Retropaint <darkglasses1122@gmail.com>
2025-03-30 14:18:16 +0700

    more ui color stuff

--- src/ui.rs
+++ src/ui.rs
@@ -8,7 +8,8 @@ use crate::shared::*;
 use crate::{armature_window, bone_window, keyframe_editor, Vec2};
 
 // UI colors
-pub const COLOR_ACCENT: egui::Color32 = egui::Color32::from_rgb(65, 43, 87);
+pub const COLOR_ACCENT: egui::Color32 = egui::Color32::from_rgb(60, 60, 60);
+pub const COLOR_MAIN: egui::Color32 = egui::Color32::from_rgb(30, 30, 30);
 
 /// The `main` of this module.
 pub fn draw(context: &Context, shared: &mut Shared) {
@@ -44,9 +45,13 @@ pub fn draw(context: &Context, shared: &mut Shared) {
                         let mut col = COLOR_ACCENT;
                         if shared.edit_mode == $mode {
                             let add = 20;
-                            col = egui::Color32::from_rgb(col.r() + add, col.g() + add, col.b() + add);
+                            col = egui::Color32::from_rgb(
+                                col.r() + add,
+                                col.g() + add,
+                                col.b() + add,
+                            );
                         }
-                        if ui.add(egui::Button::new($name).fill(col)).clicked() {
+                        if ui.add(egui::Button::new($name).fill(col).corner_radius(egui::CornerRadius::ZERO)).clicked() {
                             shared.edit_mode = $mode;
                         }
                     };
@@ -57,44 +62,50 @@ pub fn draw(context: &Context, shared: &mut Shared) {
             });
         });
 }
- fn top_panel(egui_ctx: &Context, shared: &mut Shared) {
+fn top_panel(egui_ctx: &Context, shared: &mut Shared) {
     let mut visuals = egui::Visuals::dark();
-    visuals.panel_fill = egui::Color32::from_rgb(36, 24, 43);
+    visuals.panel_fill = COLOR_MAIN;
     egui_ctx.set_visuals(visuals);
-    egui::TopBottomPanel::top("test").show(egui_ctx, |ui| {
-        egui::menu::bar(ui, |ui| {
-            ui.menu_button("File", |_ui| {});
-            ui.menu_button("View", |ui| {
-                if shared.input.mouse_left != -1 && !ui.rect_contains_pointer(ui.min_rect()) {
-                    ui.close_menu();
-                }
-
-                ui.horizontal(|ui| {
-                    ui.set_max_width(80.);
-                    if ui.button("Zoom in").clicked() {
-                        set_zoom(shared.zoom - 0.1, shared);
+    egui::TopBottomPanel::top("test")
+        .frame(egui::Frame {
+            fill: COLOR_MAIN,
+            stroke: Stroke::new(0., COLOR_ACCENT),
+            ..Default::default()
+        })
+        .show(egui_ctx, |ui| {
+            egui::menu::bar(ui, |ui| {
+                ui.menu_button("File", |_ui| {});
+                ui.menu_button("View", |ui| {
+                    if shared.input.mouse_left != -1 && !ui.rect_contains_pointer(ui.min_rect()) {
                         ui.close_menu();
                     }
 
-                    ui.with_layout(egui::Layout::right_to_left(egui::Align::Center), |ui| {
-                        ui.label("=");
+                    ui.horizontal(|ui| {
+                        ui.set_max_width(80.);
+                        if ui.button("Zoom in").clicked() {
+                            set_zoom(shared.zoom - 0.1, shared);
+                            ui.close_menu();
+                        }
+
+                        ui.with_layout(egui::Layout::right_to_left(egui::Align::Center), |ui| {
+                            ui.label("=");
+                        });
                     });
+                    ui.horizontal(|ui| {
+                        ui.set_max_width(80.);
+                        if ui.button("Zoom out").clicked() {
+                            set_zoom(shared.zoom + 0.1, shared);
+                            ui.close_menu();
+                        }
+
+                        ui.with_layout(egui::Layout::right_to_left(egui::Align::Center), |ui| {
+                            ui.label("-");
+                        });
+                    })
                 });
-                ui.horizontal(|ui| {
-                    ui.set_max_width(80.);
-                    if ui.button("Zoom out").clicked() {
-                        set_zoom(shared.zoom + 0.1, shared);
-                        ui.close_menu();
-                    }
-
-                    ui.with_layout(egui::Layout::right_to_left(egui::Align::Center), |ui| {
-                        ui.label("-");
-                    });
-                })
+                shared.ui.edit_bar_pos.y = ui.min_rect().bottom();
             });
-            shared.ui.edit_bar_pos.y = ui.min_rect().bottom();
         });
-    });
 }
 
 /// Default styling to apply across all UI.
@@ -104,7 +115,9 @@ pub fn default_styling(context: &Context) {
     // remove rounded corners on windows
     visuals.window_corner_radius = egui::CornerRadius::ZERO;
 
-    let main_color = egui::Color32::from_rgb(46, 31, 56);
+    visuals.window_fill = COLOR_MAIN;
+    visuals.panel_fill = COLOR_MAIN;
+    visuals.window_stroke = egui::Stroke::new(1., COLOR_ACCENT);
 
     context.set_visuals(visuals);
 }
@@ -122,6 +135,6 @@ pub fn set_zoom(zoom: f32, shared: &mut Shared) {
     }
 }
 
-pub fn button(text: &str, ui: &mut egui::Ui) -> egui::Response{
-    ui.add(egui::Button::new(text).fill(COLOR_ACCENT))
+pub fn button(text: &str, ui: &mut egui::Ui) -> egui::Response {
+    ui.add(egui::Button::new(text).fill(COLOR_ACCENT).corner_radius(egui::CornerRadius::ZERO))
 }
