674d4506cf522af693e4fd4284205977e0e09c31
Retropaint <darkglasses1122@gmail.com>
2025-04-21 00:29:25 +0700

    WIP visualizing verts (unused for now)

--- src/utils.rs
+++ src/utils.rs
@@ -6,8 +6,15 @@ use std::io::{Read, Write};
 /// Convert a point from screen to world space.
 pub fn screen_to_world_space(pos: Vec2, window: Vec2) -> Vec2 {
     Vec2 {
-        x: -1. + ((pos.x / window.x as f32) * 2.),
-        y: -(-1. + ((pos.y / window.y as f32) * 2.)),
+        x: -1. + (pos.x / window.x as f32 * 2.),
+        y: -(-1. + (pos.y / window.y as f32 * 2.)),
+    }
+}
+
+pub fn world_to_screen_space(pos: Vec2, window: Vec2) -> Vec2 {
+    Vec2 {
+        x: ((pos.x + 1.) * window.x as f32 / 4.),
+        y: -((pos.y - 1.) * window.y as f32 / 4.),
     }
 }
 
