f33b6e1f751e01911d2382094c4c1e685dffacaa
Retropaint <darkglasses1122@gmail.com>
2025-05-20 09:58:39 +0700

    added args to build_web.py

--- build_web.py
+++ build_web.py
@@ -1,13 +1,22 @@
 import subprocess
 import shutil
 import sys
+import argparse
+
+parser = argparse.ArgumentParser(prog='SkelForm Web Builder', description='Build script for SkelForm\'s web (WASM) version.')
+parser.add_argument('-s', '--serve', action='store_true')
+parser.add_argument('-r', '--release', action='store_true')
+args = parser.parse_args()
 
 # build /dist via trunk
-subprocess.run("trunk build --features webgl --filehash false".split()) 
+extra = ""
+if args.release:
+    extra = "--public-url /skelform_web"
+subprocess.run(("trunk build --features webgl --filehash false " + extra).split()) 
 
 # copy assets over to /dist
 shutil.copy("anim_icons.png", "dist/anim_icons.png")
 
-if len(sys.argv) > 1 and sys.argv[1] == "--serve":
+if args.serve:
     # automatically serve via python http
-    subprocess.run("python3 -m http.server 8000".split())
+    subprocess.run("python3 -m http.server 8000 --directory dist".split())
