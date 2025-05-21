a5359a41eabd01c71ab20d80b3b902a30c64fecb
Retropaint <darkglasses1122@gmail.com>
2025-05-21 10:16:24 +0700

    updated build script

--- web_build.py
+++ web_build.py
@@ -3,20 +3,26 @@ import shutil
 import sys
 import argparse
 
-parser = argparse.ArgumentParser(prog='SkelForm Web Builder', description='Build script for SkelForm\'s web (WASM) version.')
+default_build = "--features webgpu --filehash false";
+
+parser = argparse.ArgumentParser(prog='SkelForm Web Builder', description='Build script for SkelForm\'s web (WASM) version.', epilog="Default build command:\ntrunk build " + default_build)
 
 # arguments
 parser.add_argument('-s', '--serve', action='store_true', help="(bool) Automatically run localhost:8000 after build.")
-parser.add_argument('-e', '--extra', default="", help="(string) Will be appended to trunk build. Must be enclosed in \"\".")
+parser.add_argument('-b', '--build', default="", help="(string) Will be appended to trunk build. ex: --build \" --release\"")
+parser.add_argument('-nd', '--no-default', action='store_true', help="(string) If true, Trunk won't use the default build config. Can be combined with --build for fully custom builds.")
 
 args = parser.parse_args()
 
-# use a default config if no extras were provided
-if args.extra == "":
-    args.extra = "--features webgpu --filehash false" 
+# use default config if appropriate
+if args.build == "" or not args.no_default:
+    args.build = default_build + args.build
+
+build_command = "trunk build " + args.build.strip()
+print("\nBuild command:\n" + build_command + "\n")
 
 # build /dist via trunk
-subprocess.run(("trunk build " + args.extra).split()) 
+subprocess.run(build_command.split()) 
 
 # copy assets over to /dist
 shutil.copy("anim_icons.png", "dist/anim_icons.png")
