9604da85a5f0fb111e905b8da19f5851712b3f81
Retropaint <darkglasses1122@gmail.com>
2025-05-26 15:57:40 +0700

    added mobile as an official build feature!

--- web_build.py
+++ web_build.py
@@ -3,30 +3,36 @@ import shutil
 import sys
 import argparse
 
-default_build = "--features webgpu --filehash false";
+# build params (to be combined later)
+features = "\"webgpu"
+generic = " --filehash false";
+default_build = features + generic
 
 parser = argparse.ArgumentParser(prog='SkelForm Web Builder', description='Build script for SkelForm\'s web (WASM) version.', epilog="Default build command:\ntrunk build " + default_build)
 
 # arguments
 parser.add_argument('-s', '--serve', action='store_true', help="Automatically run localhost:8000 after build.")
 parser.add_argument('-r', '--release', action='store_true', help="Use default release config.")
+parser.add_argument('-m', '--mobile', action='store_true', help="Use default mobile config.")
 parser.add_argument('-b', '--build', default="", help="Will be appended to trunk build. ex: --build \" --release\"")
 parser.add_argument('-nd', '--no-default', action='store_true', help="Ignore default config. Combine with --build for fully custom builds.")
 
 args = parser.parse_args()
 
 # use default config if appropriate
-if args.build == "" or not args.no_default:
-    args.build = default_build + args.build
-
 if args.release:
-    args.build += " --release --public-url=/skelform_web"
+    generic += " --release --public-url=/skelform_web"
 
-build_command = "trunk build " + args.build.strip()
+if args.mobile:
+    features += " mobile"
+
+features += "\""
+
+build_command = "trunk build --features " + features + generic
 print("\nBuild command:\n" + build_command + "\n")
 
 # build /dist via trunk
-subprocess.run(build_command.split()) 
+subprocess.run(build_command, shell=True)
 
 # copy assets over to /dist
 shutil.copy("anim_icons.png", "dist/anim_icons.png")
