0466be94f72cd616cf061a2fb4a8c593d8ab0f7e
Retropaint <darkglasses1122@gmail.com>
2025-05-26 21:21:40 +0700

    added undo/redo to top panel

--- src/input.rs
+++ src/input.rs
@@ -2,20 +2,17 @@
 
 use crate::*;
 
-use winit::event::ElementState;
 use winit::keyboard::*;
 
 pub fn keyboard_shortcuts(shared: &mut Shared) {
     let camera_zoom_speed = 0.05;
     let ui_zoom_speed = 0.01;
 
-    let mut undo = false;
-    let mut redo = false;
     if shared.input.is_pressing(KeyCode::SuperLeft) {
         if shared.input.pressed(KeyCode::KeyZ) && shared.undo_actions.len() != 0 {
-            undo = true;
+            utils::undo_redo(true, shared);
         } else if shared.input.pressed(KeyCode::KeyY) && shared.redo_actions.len() != 0 {
-            redo = true;
+            utils::undo_redo(false, shared);
         }
 
         #[cfg(not(target_arch = "wasm32"))]
@@ -29,63 +26,6 @@ pub fn keyboard_shortcuts(shared: &mut Shared) {
             ui::set_zoom(shared.camera.zoom - camera_zoom_speed, shared);
         } else if shared.input.pressed(KeyCode::Minus) {
             ui::set_zoom(shared.camera.zoom + camera_zoom_speed, shared);
-        }
-    }
-
-    if undo || redo {
-        let action: Action;
-        if undo {
-            action = shared.undo_actions.last().unwrap().clone();
-        } else {
-            action = shared.redo_actions.last().unwrap().clone();
-        }
-        let mut new_action = action.clone();
-
-        match &action.action {
-            ActionEnum::Bone => {
-                if action.action_type == ActionType::Created {
-                    shared.selected_bone_idx = usize::MAX;
-                    if undo {
-                        for (i, bone) in shared.armature.bones.iter().enumerate() {
-                            if bone.id == action.id {
-                                shared.armature.bones.remove(i);
-                                break;
-                            }
-                        }
-                    } else {
-                        armature_window::new_bone(shared, -1);
-                    }
-                } else {
-                    new_action.bone = shared.armature.bones[action.id as usize].clone();
-                    *shared.find_bone_mut(action.id).unwrap() = action.bone.clone();
-
-                    for i in 0..shared.armature.bones.len() {
-                        shared.organize_bone(i);
-                    }
-                }
-            }
-            ActionEnum::Animation => {
-                if action.action_type == ActionType::Created {
-                    shared.ui.anim.selected = usize::MAX;
-                    if undo {
-                        shared.armature.animations.pop();
-                    } else {
-                        keyframe_editor::new_animation(shared);
-                    }
-                } else {
-                    new_action.animation = shared.armature.animations[action.id as usize].clone();
-                    shared.armature.animations[action.id as usize] = action.animation.clone();
-                }
-            }
-            _ => {}
-        }
-
-        if undo {
-            shared.redo_actions.push(new_action);
-            shared.undo_actions.pop();
-        } else {
-            shared.undo_actions.push(new_action);
-            shared.redo_actions.pop();
         }
     }
 
