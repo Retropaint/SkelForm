98dfa92c35480e46b1c63350d1c17933fdb95948
Retropaint <darkglasses1122@gmail.com>
2025-06-13 21:44:50 +0700

    moved animate() and find_connecting_frames() to armature impl

--- src/bone_panel.rs
+++ src/bone_panel.rs
@@ -98,7 +98,7 @@ pub fn draw(ui: &mut egui::Ui, shared: &mut Shared) {
 
     let mut bone = shared.selected_bone().unwrap().clone();
     if shared.ui.anim.open && shared.ui.anim.selected != usize::MAX {
-        bone = shared.animate(shared.ui.anim.selected)[shared.selected_bone_idx].clone();
+        bone = shared.armature.animate(shared.ui.anim.selected, shared.ui.anim.selected_frame)[shared.selected_bone_idx].clone();
     }
 
     let mut edited = false;
