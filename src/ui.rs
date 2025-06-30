6af5eba1b08f4cb73f0e30411dd6f4a65fc2cef4
Retropaint <darkglasses1122@gmail.com>
2025-06-30 17:06:36 +0700

    added documentation top bar button

--- src/ui.rs
+++ src/ui.rs
@@ -243,6 +243,19 @@ fn top_panel(egui_ctx: &Context, shared: &mut Shared) {
                         }
                         ui.close_menu();
                     }
+                    if top_bar_button(ui, "Documentation", "", &mut offset).clicked() {
+                        // open the local docs, or online if it can't be found on default path
+                        match open::that("./user_docs/index.html") {
+                            Err(_) => {
+                                match open::that("https://retropaint.github.io/skelform_user_docs")
+                                {
+                                    Err(_) => println!("couldn't open"),
+                                    Ok(file) => file,
+                                }
+                            }
+                            Ok(file) => file,
+                        };
+                    }
                 });
             });
 
