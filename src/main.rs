fc1f9278bfacacc61a59af89e86e52fd1c2c4aac
Retropaint <darkglasses1122@gmail.com>
2025-07-02 01:48:21 +0700

    load armature via first arg

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,10 @@ use skelform_lib::shared::*;
 use skelform_lib::file_reader;
 
 fn main() -> Result<(), winit::error::EventLoopError> {
+    // delete any leftover temporary files
+    #[cfg(not(target_arch = "wasm32"))]
+    file_reader::del_temp_files();
+
     // uncomment below to get console panic hook as early as possible for debugging
     //
     // otherwise, it's activated in lib.rs
@@ -17,15 +21,21 @@ fn main() -> Result<(), winit::error::EventLoopError> {
     //    log::info!("test");
     //}
 
+    let args: Vec<String> = std::env::args().collect();
+
+    // load .skf based on first arg
+    if args.len() > 1 {
+        file_reader::create_temp_file(
+            &skelform_lib::bone_panel::TEMP_IMPORT_PATH,
+            &args[1].to_string(),
+        );
+    }
+
     let mut app = skelform_lib::App::default();
     init_shared(&mut app.shared);
 
     let event_loop = winit::event_loop::EventLoop::builder().build()?;
     event_loop.set_control_flow(winit::event_loop::ControlFlow::Poll);
-
-    // delete any leftover temporary files
-    #[cfg(not(target_arch = "wasm32"))]
-    file_reader::del_temp_files();
 
     event_loop.run_app(&mut app)
 }
@@ -39,7 +49,7 @@ fn init_shared(shared: &mut Shared) {
     shared.ui.anim.timeline_zoom = 1.;
     shared.ui.anim.exported_frame = "".to_string();
     shared.ui.anim.selected_frame = -1;
-    shared.ui.anim.dragged_keyframe = Keyframe{
+    shared.ui.anim.dragged_keyframe = Keyframe {
         frame: -1,
         ..Default::default()
     };
