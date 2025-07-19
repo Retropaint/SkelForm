2a2d2ab5d6406fc8a99c3353e95747d0f2b576cf
Retropaint <darkglasses1122@gmail.com>
2025-07-20 05:58:34 +0700

    improved release script

--- release.py
+++ release.py
@@ -10,6 +10,28 @@ import os
 import platform
 import shutil
 
+RED = "\033[31m"
+RESET = "\033[0m"
+
+
+def require_docs(header, doc_name):
+    print(f"{RED}!! {header} DOCUMENTATION REQUIRED !!{RESET}")
+    print(f"1. Build it - https://github.com/retropaint/skelform_{doc_name}")
+    print(f"2. Move `book` dir here and rename to '{doc_name}'")
+    print("")
+
+
+# Require user & dev docs before building
+can_build = True
+if not os.path.exists("user_docs"):
+    require_docs("USER", "user_docs")
+    can_build = False
+if not os.path.exists("dev_docs"):
+    require_docs("DEV", "dev_docs")
+    can_build = False
+if not can_build:
+    exit()
+
 binExt = ".exe" if platform.system == "Windows" else ""
 
 platform_name = ""
@@ -22,65 +44,39 @@ match platform.system():
         platform_name = "linux"
 
 version = ""
-
 with open("cargo.toml", "r") as file:
-    all_lines = file.readlines()
-    for line in all_lines:
+    for line in file.readlines():
         if "version" in line.strip():
             version = line.strip().split('"')[1]
             break
 
 dirname = "skelform_" + platform_name + "_v" + version
 
-can_build = True
-RED = "\033[31m"
-RESET = "\033[0m"
-# Very politely ask for the user docs distribution
-if not os.path.exists("user_docs"):
-    print(f"{RED}!! USER DOCUMENTATION REQUIRED !!{RESET}")
-    print("1. Build it - https://github.com/Retropaint/skelform_user_docs")
-    print("2. Move `book` dir here and rename to 'user_docs'")
-    print("")
-    can_build = False
-
-# Very politely ask for the dev docs distribution
-if not os.path.exists("dev_docs"):
-    print(f"{RED}!! DEVELOPER DOCUMENTATION REQUIRED !!{RESET}")
-    print("1. Build it - https://github.com/Retropaint/skelform_dev_docs")
-    print("2. Move `book` dir here and rename to 'dev_docs'")
-    can_build = False
-
-if not can_build:
-    exit()
-
-# remove dist folder if it already exists
 if os.path.exists(dirname):
     shutil.rmtree(dirname)
-
-# create dist folder, where everything will go
 os.mkdir(dirname)
 
-# create binary
-build_command = "cargo build --release"
-subprocess.run(build_command, shell=True)
-
-# move binary to dist
+subprocess.run("cargo build --release", shell=True)
 shutil.copy("./target/release/SkelForm" + binExt, "./" + dirname)
-
 shutil.copytree("./user_docs", "./" + dirname + "/user_docs")
 shutil.copytree("./dev_docs", "./" + dirname + "/dev_docs")
 shutil.copy("./readme.txt", "./" + dirname)
 
 # Source code distribution
 
-# create source code folder
 source = dirname + "/source"
 os.mkdir("./" + source)
-
-# copy relevant stuff
 shutil.copy("Cargo.toml", "./" + source)
 shutil.copy("release.py", "./" + source)
 shutil.copy("web_build.py", "./" + source)
 shutil.copy("anim_icons.png", "./" + source)
 shutil.copy("readme.md", "./" + source)
 shutil.copytree("src", "./" + source + "/src")
+
+# Platform-specific distribution
+
+if platform.system() == "Darwin":
+    bin_path = "./release_wrappers/skelform.app/Contents/MacOS/"
+    if os.path.exists(bin_path):
+        shutil.rmtree(bin_path)
+    shutil.copytree(dirname, "./release_wrappers/skelform.app/Contents/MacOS/")
