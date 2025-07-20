666abcd9e82a365974272aa0a6c923cfc241d13d
Retropaint <darkglasses1122@gmail.com>
2025-07-20 07:32:38 +0700

    added create-dmg.sh script

---
+++ create-dmg.sh
@@ -0,0 +1,8 @@
+create-dmg \
+  --background dmg_bg.png \
+  --window-pos 200 200 \
+  --window-size 600 300 \
+  --icon-size 100 \
+  --icon SkelForm.app 0 125 \
+  --hide-extension "SkelForm.app" \
+  --app-drop-link 350 125 \ skelform.dmg mac_wrapper
