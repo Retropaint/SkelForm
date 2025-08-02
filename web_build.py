065ed3198bd4797133fde5f028906f39e420e9dd
Retropaint <darkglasses1122@gmail.com>
2025-08-02 13:20:04 +0700

    added trunk check in web build script

--- web_build.py
+++ web_build.py
@@ -3,19 +3,46 @@ import shutil
 import sys
 import argparse
 
+RED = "\033[31m"
+RESET = "\033[0m"
+
+if not shutil.which("trunk"):
+    print(f">>> {RED}!! trunk REQUIRED !!{RESET}")
+    print(">>> Install trunk - https://trunkrs.dev/")
+    print("")
+    exit()
+
 # build params (to be combined later)
-features = "\"webgpu"
-generic = " --filehash false";
+features = '"webgpu'
+generic = " --filehash false"
 default_build = features + generic
 
-parser = argparse.ArgumentParser(prog='SkelForm Web Builder', description='Build script for SkelForm\'s web (WASM) version.', epilog="Default build command:\ntrunk build " + default_build)
+parser = argparse.ArgumentParser(
+    prog="SkelForm Web Builder",
+    description="Build script for SkelForm's web (WASM) version.",
+    epilog="Default build command:\ntrunk build " + default_build,
+)
 
 # arguments
-parser.add_argument('-s', '--serve', action='store_true', help="automatically run localhost:8000 after build")
-parser.add_argument('-r', '--release', action='store_true', help="build for release/production")
-parser.add_argument('-m', '--mobile', action='store_true', help="build for mobile")
-parser.add_argument('-d', '--debug', action='store_true', help="build with debug flag. Ignored if --release is present")
-parser.add_argument('-wgl', '--webgl', action='store_true', help="use webgl instead of webgpu")
+parser.add_argument(
+    "-s",
+    "--serve",
+    action="store_true",
+    help="automatically run localhost:8000 after build",
+)
+parser.add_argument(
+    "-r", "--release", action="store_true", help="build for release/production"
+)
+parser.add_argument("-m", "--mobile", action="store_true", help="build for mobile")
+parser.add_argument(
+    "-d",
+    "--debug",
+    action="store_true",
+    help="build with debug flag. Ignored if --release is present",
+)
+parser.add_argument(
+    "-wgl", "--webgl", action="store_true", help="use webgl instead of webgpu"
+)
 
 args = parser.parse_args()
 
@@ -24,14 +51,14 @@ if args.release and not args.mobile:
     generic += " --release --public-url=/skelform_web"
 
 if args.webgl:
-    features = "\"webgl"
+    features = '"webgl'
 
 if args.mobile:
     features += " mobile"
 if args.debug and not args.release:
     features += " debug"
 
-features += "\""
+features += '"'
 
 build_command = "trunk build --features " + features + generic
 print("\nBuild command:\n" + build_command + "\n")
