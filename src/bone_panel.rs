d087a3bd07e092e217900664ce13e8abad15c45a
Retropaint <darkglasses1122@gmail.com>
2025-08-26 14:10:48 +0700

    editing bone panel now autosaves

--- src/bone_panel.rs
+++ src/bone_panel.rs
@@ -182,6 +182,7 @@ pub fn draw(ui: &mut egui::Ui, shared: &mut Shared) {
                     anim_id,
                     shared.ui.anim.selected_frame,
                 );
+                shared.armature.autosave();
             }
             if $label != "" {
                 $ui.label($label);
