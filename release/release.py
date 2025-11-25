175443d2c8c1ab262ffd5652314934af66a30aed
Retropaint <darkglasses1122@gmail.com>
2025-11-25 17:03:46 +0700

    locked mdbook to 0.5.1

--- release/release.py
+++ release/release.py
@@ -25,7 +25,7 @@ args = parser.parse_args()
 
 stdout = "" if args.verbose else " &> /dev/null"
 
-subprocess.run("cargo install mdbook", shell=True)
+subprocess.run("cargo install mdbook@0.5.1", shell=True)
 subprocess.run("git clone https://github.com/Retropaint/skelform_dev_docs", shell=True)
 subprocess.run("git clone https://github.com/Retropaint/skelform_user_docs", shell=True)
 subprocess.run("mdbook build skelform_dev_docs", shell=True)
