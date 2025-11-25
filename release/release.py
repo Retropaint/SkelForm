f4c1cbf206dd6eb30e2dfc4c4f9246c8b646d6d7
Retropaint <darkglasses1122@gmail.com>
2025-11-25 15:59:48 +0700

    release script now just clones and builds docs

--- release/release.py
+++ release/release.py
@@ -21,44 +21,21 @@ parser = argparse.ArgumentParser(prog="SkelForm Release Builder", description="B
 parser.add_argument("-v", "--verbose", action="store_true", help="Print output of everything")
 parser.add_argument("-dmg", "--dmg", action="store_true", help="Attempt to create Mac dmg (requires create-dmg)")
 parser.add_argument("-dbg", "--debug", action="store_true", help="Create debug build")
-parser.add_argument("-d", "--docs", type=str, help=f"Build and move user & dev docs here. `user_docs` and `dev_docs` will be appended to the input dir.\n{CYAN}Example{RESET}: -docs ../../skelform_")
-parser.add_argument("-nd", "--nodocs", action="store_true", help="Ignore docs for packaging")
 args = parser.parse_args()
 
 stdout = "" if args.verbose else " &> /dev/null"
 
-if args.docs:
-    subprocess.run ("mdbook build " + args.docs + "user_docs", shell=True)
-    shutil.copytree(f"{args.docs}user_docs/book", "./user-docs", dirs_exist_ok = True)
-    subprocess.run ("mdbook build " + args.docs + "dev_docs", shell=True)
-    shutil.copytree(f"{args.docs}dev_docs/book", "./dev-docs", dirs_exist_ok = True)
-
-def require_docs(header, doc_name):
-    print(f">>> {RED}!! {header} DOCUMENTATION REQUIRED !!{RESET}")
-    print(f">>> 1. Build it - https://github.com/retropaint/skelform_{doc_name}")
-    print(f">>> 2. Move `book` dir here and rename to '{doc_name}'")
-    print("")
-
-
-# Require user & dev docs before building
-can_build = True
-user_docs = "user-docs"
-dev_docs = "dev-docs"
-if not os.path.exists(user_docs) and not args.nodocs:
-    require_docs("USER", user_docs)
-    can_build = False
-if not os.path.exists(user_docs) and not args.nodocs:
-    require_docs("DEV", user_docs)
-    can_build = False
-
+subprocess.run("git clone https://github.com/Retropaint/skelform_dev_docs", shell=True)
+subprocess.run("git clone https://github.com/Retropaint/skelform_user_docs", shell=True)
+subprocess.run("mdbook build skelform_dev_docs", shell=True)
+subprocess.run("mdbook build skelform_user_docs", shell=True)
+shutil.copytree("skelform_dev_docs/book", "./dev-docs", dirs_exist_ok = True)
+shutil.copytree("skelform_user_docs/book", "./user-docs", dirs_exist_ok = True)
 
 # Require create-dmg on mac
 if platform.system() == "Darwin" and not shutil.which("create-dmg") and args.dmg:
     print(f">>> {RED}!! create-dmg REQUIRED !!{RESET}")
     print(">>> Install create-dmg - https://github.com/create-dmg/create-dmg")
-    can_build = False
-
-if not can_build:
     exit()
 
 binExt = ".exe" if platform.system() == "Windows" else ""
@@ -94,9 +71,8 @@ if args.debug:
 # yapf: disable
 subprocess.run (f"cargo build {mode}", shell=True)
 shutil.copy    (f"../target/{path}/SkelForm{binExt}", f"./{dirname}")
-if not args.nodocs:
-    shutil.copytree(f"./{user_docs}", f"./{dirname}/{user_docs}")
-    shutil.copytree(f"./{dev_docs}",  f"./{dirname}/{dev_docs}")
+shutil.copytree("./user-docs", f"./{dirname}/user-docs")
+shutil.copytree("./dev-docs",  f"./{dirname}/dev-docs")
 shutil.copytree("../assets",      f"./{dirname}/assets")
 shutil.copytree("../samples",     f"./{dirname}/samples")
 
