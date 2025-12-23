0ed94438a7d7f1e669993be6d0d4146be0937765
Retropaint <darkglasses1122@gmail.com>
2025-12-23 18:17:53 +0700

    removed debug fields from ditto

--- src/main.rs
+++ src/main.rs
@@ -88,8 +88,6 @@ fn init_shared(shared: &mut Shared) {
     shared.screenshot_res = Vec2::new(128., 128.);
     shared.changed_vert_id = -1;
     //shared.ui.atlas_modal = true;
-    shared.open_style_modal();
-    shared.ui.selected_style = 0;
 
     #[cfg(feature = "debug")]
     {
