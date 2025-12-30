a3040c2a23856cdb9bf8828a4ddb8168861a3f13
Retropaint <darkglasses1122@gmail.com>
2025-12-31 01:29:14 +0700

    added --nodocs for release script

--- release/release.py
+++ release/release.py
@@ -21,17 +21,23 @@ parser = argparse.ArgumentParser(prog="SkelForm Release Builder", description="B
 parser.add_argument("-v", "--verbose", action="store_true", help="Print output of everything")
 parser.add_argument("-dmg", "--dmg", action="store_true", help="Attempt to create Mac dmg (requires create-dmg)")
 parser.add_argument("-dbg", "--debug", action="store_true", help="Create debug build")
+parser.add_argument("-nd", "--nodocs", action="store_true", help="Skip user docs & dev docs")
 args = parser.parse_args()
 
 stdout = "" if args.verbose else " &> /dev/null"
 
-subprocess.run("cargo install mdbook@0.5.1", shell=True)
-subprocess.run("git clone https://github.com/Retropaint/skelform_dev_docs", shell=True)
-subprocess.run("git clone https://github.com/Retropaint/skelform_user_docs", shell=True)
-subprocess.run("mdbook build skelform_dev_docs", shell=True)
-subprocess.run("mdbook build skelform_user_docs", shell=True)
-shutil.copytree("skelform_dev_docs/book", "./dev-docs", dirs_exist_ok = True)
-shutil.copytree("skelform_user_docs/book", "./user-docs", dirs_exist_ok = True)
+if not args.nodocs:
+    shutil.rmtree("skelform_dev_docs", ignore_errors=True)
+    shutil.rmtree("skelform_user_docs", ignore_errors=True)
+    shutil.rmtree("user_docs", ignore_errors=True)
+    shutil.rmtree("dev_docs", ignore_errors=True)
+    subprocess.run("cargo install mdbook@0.5.1", shell=True)
+    subprocess.run("git clone https://github.com/Retropaint/skelform_dev_docs", shell=True)
+    subprocess.run("git clone https://github.com/Retropaint/skelform_user_docs", shell=True)
+    subprocess.run("mdbook build skelform_dev_docs", shell=True)
+    subprocess.run("mdbook build skelform_user_docs", shell=True)
+    shutil.copytree("skelform_dev_docs/book", "./dev-docs", dirs_exist_ok = True)
+    shutil.copytree("skelform_user_docs/book", "./user-docs", dirs_exist_ok = True)
 
 # Require create-dmg on mac
 if platform.system() == "Darwin" and not shutil.which("create-dmg") and args.dmg:
@@ -72,8 +78,9 @@ if args.debug:
 # yapf: disable
 subprocess.run (f"cargo build {mode}", shell=True)
 shutil.copy    (f"../target/{path}/SkelForm{binExt}", f"./{dirname}")
-shutil.copytree("./user-docs", f"./{dirname}/user-docs")
-shutil.copytree("./dev-docs",  f"./{dirname}/dev-docs")
+if not args.nodocs:
+    shutil.copytree("./user-docs", f"./{dirname}/user-docs")
+    shutil.copytree("./dev-docs",  f"./{dirname}/dev-docs")
 shutil.copytree("../assets",      f"./{dirname}/assets")
 shutil.copytree("../samples",     f"./{dirname}/samples")
 
