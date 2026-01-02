c150e476bc44c505a4bf94a556cd75e940950dfa
Retropaint <darkglasses1122@gmail.com>
2026-01-02 23:56:43 +0700

    changed bgra to rgba for screenshots

--- src/lib.rs
+++ src/lib.rs
@@ -696,7 +696,7 @@ impl Renderer {
             mip_level_count: 1,
             sample_count: 1,
             dimension: wgpu::TextureDimension::D2,
-            format: wgpu::TextureFormat::Bgra8Unorm,
+            format: wgpu::TextureFormat::Rgba8Unorm,
             usage: wgpu::TextureUsages::RENDER_ATTACHMENT | wgpu::TextureUsages::COPY_SRC,
             label: Some("Capture Texture"),
             view_formats: &[],
