ff5a8320756e82d44b84c40a5f5c065c62904929
Retropaint <darkglasses1122@gmail.com>
2026-02-19 00:21:15 +0700

    test commit

--- src/shared.rs
+++ src/shared.rs
@@ -2579,4 +2579,4 @@ pub fn recents_path() -> std::path::PathBuf {
     directories_next::ProjectDirs::from("com", "retropaint", "skelform")
         .map(|proj_dirs| proj_dirs.data_dir().join("recent_files.json"))
         .unwrap()
-}
+} 
