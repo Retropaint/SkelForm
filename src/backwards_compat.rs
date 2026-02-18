f5dfb54d7fb5c47d83a2665b773ec2a2c52b9864
Retropaint <darkglasses1122@gmail.com>
2026-02-18 11:02:37 +0700

    v0.2 to v0.4 backwards compat

--- src/backwards_compat.rs
+++ src/backwards_compat.rs
@@ -3,8 +3,12 @@ use strum::FromRepr;
 use crate::Root;
 
 pub fn proceed(mut raw: serde_json::Value) -> Root {
-    let version = raw.get("version").unwrap().as_str().unwrap();
+    let version_raw = raw.get("version").unwrap().clone();
+    let version = version_raw.as_str().unwrap();
 
+    if version.contains("v0.2") {
+        v0d2_to_v0d3(&mut raw);
+    }
     if version.contains("v0.3") {
         v0d3_to_v0d4(&mut raw);
     }
@@ -12,8 +16,13 @@ pub fn proceed(mut raw: serde_json::Value) -> Root {
     serde_json::from_value(raw).unwrap()
 }
 
+pub fn v0d2_to_v0d3(raw: &mut serde_json::Value) {
+    raw["version"] = "v0.3.0".into();
+}
+
 pub fn v0d3_to_v0d4(raw: &mut serde_json::Value) {
     for bone in raw.get_mut("bones").iter_mut() {
         bone["hidden"] = bone["is_hidden"].clone();
     }
+    raw["version"] = "v0.3.0".into();
 }
