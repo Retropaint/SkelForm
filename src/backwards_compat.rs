59c5d087333800f40c866fc46f27a51f88242620
Retropaint <darkglasses1122@gmail.com>
2026-02-18 10:57:55 +0700

    change is_hidden to hidden from v0.3 to v0.4

--- src/backwards_compat.rs
+++ src/backwards_compat.rs
@@ -2,7 +2,18 @@ use strum::FromRepr;
 
 use crate::Root;
 
-pub fn proceed(raw: serde_json::Value) -> Root {
+pub fn proceed(mut raw: serde_json::Value) -> Root {
     let version = raw.get("version").unwrap().as_str().unwrap();
+
+    if version.contains("v0.3") {
+        v0d3_to_v0d4(&mut raw);
+    }
+
     serde_json::from_value(raw).unwrap()
+}
+
+pub fn v0d3_to_v0d4(raw: &mut serde_json::Value) {
+    for bone in raw.get_mut("bones").iter_mut() {
+        bone["hidden"] = bone["is_hidden"].clone();
+    }
 }
